aa5758148afda8e8a56cd0c4ada44bbc742c11be
alexandre-andre <andregpf89@gmail.com>
2022-08-01 11:49:15 -0300

    build: rota /teams

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import loginRouter from './login-router';
+import teamsRouters from './teams-router';
 
 const routes: Router = Router();
 
 routes.use('/login', loginRouter);
+routes.use('/teams', teamsRouters);
 
 export default routes;
---
+++ app/backend/src/routes/teams-router.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const teamsRouters = Router();
+
+teamsRouters('');
+
+export default teamsRouters;
