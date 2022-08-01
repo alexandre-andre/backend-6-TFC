ec0ec2b49e80e20e6ebe6a897a6b87b097f6cfea
alexandre-andre <andregpf89@gmail.com>
2022-08-01 18:17:27 -0300

    build: structure of matches routes

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import loginRouter from './login-router';
 import teamsRouters from './teams-router';
+import matchesRouters from './matches-router';
 
 const routes: Router = Router();
 
 routes.use('/login', loginRouter);
 routes.use('/teams', teamsRouters);
+routes.use('/matches', matchesRouters);
 
 export default routes;
---
+++ app/backend/src/routes/matches-router.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const matchesRouters = Router();
+
+matchesRouters.get('/');
+
+export default matchesRouters;
