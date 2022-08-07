ebb3a000257ba964f8f8b5f0c885f464c691ab38
alexandre-andre <andregpf89@gmail.com>
2022-08-07 14:27:41 -0300

    terceira tentativa req 29

---
+++ app/backend/src/controller/leaderboard-controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import LeaderboardService from '../service/leaderboard-service';
+
+class LeaderboardController {
+    public leaderboardService: LeaderboardService;
+
+		constructor() {
+			this.leaderboardService = new LeaderboardService();
+		}
+
+		public async getLeaderboard(_request: Request, response: Response) {
+			const result = await this.leaderboardService.getLeaderboard();
+
+			return response.status(StatusCodes.OK).json(result);
+		}
+}
+
+export default LeaderboardController;
--- app/backend/src/routes/leaderboard-router.ts
+++ app/backend/src/routes/leaderboard-router.ts
@@ -1,7 +1,12 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import LeaderboardController from '../controller/leaderboard-controller';
 
 const leaderRouter = Router();
+const leaderboardController = new LeaderboardController();
 
-leaderRouter.get('/home');
+leaderRouter.get(
+  '/home',
+  (request: Request, response: Response) => leaderboardController.getLeaderboard(request, response),
+);
 
 export default leaderRouter;
