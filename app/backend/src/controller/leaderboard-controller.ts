26e2ecb8bb28dbc5ebd3c6021c216d7c7a6bfd31
alexandre-andre <andregpf89@gmail.com>
2022-08-08 11:52:51 -0300

    build: (controller-leaderboard) function that will get ranking from visitor

--- app/backend/src/controller/leaderboard-controller.ts
+++ app/backend/src/controller/leaderboard-controller.ts
@@ -15,6 +15,12 @@ class LeaderboardController {
 
     response.status(StatusCodes.OK).json(result);
   }
+
+	public async getRankWhenVisitor(_request: Request, response: Response) {
+		const result = await this.leaderboardService.getRankWhenVisitor();
+
+    response.status(StatusCodes.OK).json(result);
+	}
 }
 
 export default LeaderboardController;
