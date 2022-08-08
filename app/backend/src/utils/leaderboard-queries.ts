8c8ce29f39627f1d9f1f02ad2a81d4f0a40538c2
alexandre-andre <andregpf89@gmail.com>
2022-08-08 20:30:27 -0300

    build: implementa alteração de string para number nas queries das partidas

--- app/backend/src/utils/leaderboard-queries.ts
+++ app/backend/src/utils/leaderboard-queries.ts
@@ -1,22 +1,22 @@
 const queryAtHome = `
   SELECT
     t.team_name AS name,
-    SUM(
+    CAST(SUM(
       3*(m.home_team_goals > m.away_team_goals) + (m.home_team_goals = m.away_team_goals)
-    ) AS totalPoints,
+    ) AS SIGNED) AS totalPoints,
     COUNT(*) AS totalGames,
-    SUM(m.home_team_goals > m.away_team_goals) AS totalVictories,
-    SUM(m.home_team_goals = m.away_team_goals) AS totalDraws,
-    SUM(m.home_team_goals < m.away_team_goals) AS totalLosses,
-    SUM(m.home_team_goals) AS goalsFavor,
-    SUM(m.away_team_goals) AS goalsOwn,
-    SUM(m.home_team_goals - m.away_team_goals) AS goalsBalance,
-    FORMAT(
+    CAST(SUM(m.home_team_goals > m.away_team_goals) AS SIGNED) AS totalVictories,
+    CAST(SUM(m.home_team_goals = m.away_team_goals) AS SIGNED) AS totalDraws,
+    CAST(SUM(m.home_team_goals < m.away_team_goals) AS SIGNED) AS totalLosses,
+    CAST(SUM(m.home_team_goals) AS SIGNED) AS goalsFavor,
+    CAST(SUM(m.away_team_goals) AS SIGNED) AS goalsOwn,
+    CAST(SUM(m.home_team_goals - m.away_team_goals) AS SIGNED) AS goalsBalance,
+    CAST(FORMAT(
       (
         SUM(3*(m.home_team_goals > m.away_team_goals) + (m.home_team_goals = m.away_team_goals))
         / (COUNT(*) * 3)
       ) * 100
-    ,2) AS efficiency
+    ,2) AS DOUBLE) AS efficiency
   FROM matches AS m
   INNER JOIN teams AS t
   ON m.home_team = t.id
@@ -31,22 +31,22 @@ const queryAtHome = `
 const queryWhenVisitor = `
   SELECT
     t.team_name AS name,
-    SUM(
+    CAST(SUM(
       3*(m.away_team_goals > m.home_team_goals) + (m.away_team_goals = m.home_team_goals)
-    ) AS totalPoints,
+    ) AS SIGNED) AS totalPoints,
     COUNT(*) AS totalGames,
-    SUM(m.away_team_goals > m.home_team_goals) AS totalVictories,
-    SUM(m.away_team_goals = m.home_team_goals) AS totalDraws,
-    SUM(m.away_team_goals < m.home_team_goals) AS totalLosses,
-    SUM(m.away_team_goals) AS goalsFavor,
-    SUM(m.home_team_goals) AS goalsOwn,
-    SUM(m.away_team_goals - m.home_team_goals) AS goalsBalance,
-    FORMAT(
+    CAST(SUM(m.away_team_goals > m.home_team_goals) AS SIGNED) AS totalVictories,
+    CAST(SUM(m.away_team_goals = m.home_team_goals) AS SIGNED) AS totalDraws,
+    CAST(SUM(m.away_team_goals < m.home_team_goals) AS SIGNED) AS totalLosses,
+    CAST(SUM(m.away_team_goals) AS SIGNED) AS goalsFavor,
+    CAST(SUM(m.home_team_goals) AS SIGNED) AS goalsOwn,
+    CAST(SUM(m.away_team_goals - m.home_team_goals) AS SIGNED) AS goalsBalance,
+    CAST(FORMAT(
       (
         SUM(3*(m.away_team_goals > m.home_team_goals) + (m.away_team_goals = m.home_team_goals))
         / (COUNT(*) * 3)
       ) * 100
-    ,2) AS efficiency
+    ,2) AS DOUBLE) AS efficiency
   FROM matches AS m
   INNER JOIN teams AS t
   ON m.away_team = t.id
